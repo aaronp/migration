479e1bcca2365e8b9adb9886234809d309f79174
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-09-23 10:24:02 +0100

    regexReplace

--- src/main/scala/migration/RegexResolve.scala
+++ src/main/scala/migration/RegexResolve.scala
@@ -1,17 +1,64 @@
 package migration
 
+import scala.util.{Failure, Success, Try}
+
 object RegexResolve {
-  def replace(fileName: String,
-              list: List[String],
-              pattern: String): _root_.scala.Predef.String = {
-    ???
+  private val BackRef = "(.*?)\\$([0-9]+)(.*)".r
+  type Group = IndexedSeq[String]
+  type Swap = Group => String
+
+  def apply(regex: String, replacement: String): String => Try[String] = {
+    (regex, replacement) match {
+      case ("", "") => (text: String) => Success(text)
+      case (regex, pattern) => {
+        val resolve = RegexResolve.forPattern(regex)
+        val replacement = RegexResolve.replace(pattern)
+        (fileName: String) => {
+          resolve(fileName) match {
+            case None => Failure(new Exception(s"'$fileName' didn't match $regex"))
+            case Some(list) => Try(replacement(list))
+          }
+        }
+      }
+    }
   }
 
-  def apply(pattern: String): CharSequence => Option[String] = {
-    forPattern(pattern).andThen(_.map(_.mkString("")))
+  def replace(replacementPattern: String): Swap = {
+    createReplacement(replacementPattern) match {
+      case Left(constant) => (_: Group) => constant
+      case Right(swap) => swap
+    }
   }
 
-  def forPattern(pattern: String): CharSequence => Option[List[String]] =
-    pattern.r.unapplySeq(_: CharSequence)
+
+  private def createReplacement(replacementPattern: String): Either[String, Swap] = {
+    def groupAt(capture: Group, index: Int): String = {
+      try {
+        capture(index)
+      } catch {
+        case _: ArrayIndexOutOfBoundsException => {
+          sys.error(s"regex only captured ${capture.size} groups, so cannot replace index $index in pattern '${replacementPattern}', captured group: ${capture.mkString("[", ",", "]")}")
+        }
+      }
+    }
+
+    replacementPattern match {
+      case BackRef(before, indexStr, after) =>
+        // back-references typically are one-based (e.g. $1 is the first group)
+        val index = indexStr.toInt - 1
+        createReplacement(after) match {
+          case Left(finished) =>
+            val swap = (group: Group) => s"$before${groupAt(group, index)}$finished"
+            Right(swap)
+          case Right(continuation) =>
+            val swap = (group: Group) => s"$before${groupAt(group, index)}${continuation(group)}"
+            Right(swap)
+        }
+      case other => Left(other)
+    }
+  }
+
+  def forPattern(pattern: String): CharSequence => Option[IndexedSeq[String]] =
+    pattern.r.unapplySeq(_: CharSequence).map(_.toIndexedSeq)
 
 }
--- src/test/scala/migration/RegexResolveTest.scala
+++ src/test/scala/migration/RegexResolveTest.scala
@@ -3,16 +3,36 @@ package migration
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.util.Success
+
 class RegexResolveTest extends AnyWordSpec with Matchers {
 
-  "RegexResolve" should {
-    "map strings" in {
-      val resolver = RegexResolve("foo([0-9]{2,3}).(.)\\.txt")
-      resolver("foo12XY.txt") shouldBe Some("12Y")
-      resolver("foo124ab.txt") shouldBe Some("124b")
-      resolver("foo124ab.tx") shouldBe None
-      resolver("bar124ab.txt") shouldBe None
-      resolver("foo1_3ab.txt") shouldBe None
+  "RegexResolve.replace" should {
+    "replace back-references from one string in another" in {
+      val replacement = RegexResolve.replace("first:$1 second:$2.txt")
+      replacement(Array("foo", "bar", "bazz")) shouldBe "first:foo second:bar.txt"
+      RegexResolve.replace("$2$1")(Array("foo", "bar")) shouldBe "barfoo"
+      RegexResolve.replace("$2")(Array("foo", "bar")) shouldBe "bar"
+    }
+    "ignore the group if there are no back-references" in {
+      RegexResolve.replace("this is constant")(Array("foo", "bar")) shouldBe "this is constant"
+      RegexResolve.replace("this is constant")(Array[String]()) shouldBe "this is constant"
+    }
+    "throw an exception if the replacement string don't capture the group" in {
+      val bang = intercept[Exception] {
+        RegexResolve.replace("$2$1")(Array("just one"))
+      }
+      bang.getMessage shouldBe "regex only captured 1 groups, so cannot replace index 1 in pattern '$2$1', captured group: [just one]"
+    }
+  }
+  "RegexResolve.apply" should {
+    "replace text in a string" in {
+      val resolver = RegexResolve("foo([0-9]{2,3}).(.)\\.txt", "$2 after $1")
+      resolver("foo12XY.txt") shouldBe Success("Y after 12")
+      resolver("foo124ab.txt") shouldBe Success("b after 124")
+      resolver("foo124ab.tx").toEither.swap.map(_.getMessage) shouldBe Right("'foo124ab.tx' didn't match foo([0-9]{2,3}).(.)\\.txt")
+      resolver("bar124ab.txt").toEither.swap.map(_.getMessage) shouldBe Right("'bar124ab.txt' didn't match foo([0-9]{2,3}).(.)\\.txt")
+      resolver("foo1_3ab.txt").toEither.swap.map(_.getMessage) shouldBe Right("'foo1_3ab.txt' didn't match foo([0-9]{2,3}).(.)\\.txt")
     }
   }
 }
