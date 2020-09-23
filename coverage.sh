7712a122b9f2d020aae27bfb122353de52a9f48d
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-09-23 17:38:46 +0100

    updated coverage

--- coverage.sh
+++ coverage.sh
@@ -2,7 +2,7 @@
 
 sbt clean coverage test coverageReport coveralls
 
-cp -r target/scala-2.12/scoverage-report target
+cp -r target/scala-2.13/scoverage-report target
 
 echo "See ./target/scoverage-report/index.html"
 open ./target/scoverage-report/index.html
