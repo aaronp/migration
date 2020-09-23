08420d736dafdf419e52ee9504e0325cfb67fd8e
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-09-23 10:54:38 +0100

    mainTestPassing

--- src/test/scala/migration/MainTest.scala
+++ src/test/scala/migration/MainTest.scala
@@ -1,10 +1,10 @@
 package migration
 
 import eie.io._
-import zio.Task
-import zio.test.Assertion.equalTo
+import zio.test.Assertion.{contains, equalTo}
 import zio.test._
 import zio.test.environment.TestConsole
+import zio.{ExitCode, Task}
 
 object MainTest extends DefaultRunnableSpec {
   override def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = {
@@ -14,25 +14,29 @@ object MainTest extends DefaultRunnableSpec {
         testDir <- Task.effect(s"./target/mainTest-${UniqueIds.next()}/data".asPath)
         exitCode <- Main.run(
           List(s"dir=${testDir}",
-               "pattern=\\2@@@\\1",
-               "regex=f(.)le(.+)\\.xml"))
-//          _ <- Task.effect(testDir.delete())
+            "pattern=\\2@@@\\1",
+            "regex=f(.)le(.+)\\.xml"))
+        //          _ <- Task.effect(testDir.delete())
         output <- TestConsole.output
       } yield {
         println("v" * 120)
         output.foreach(println)
         println("^" * 120)
         assert(output)(
-          equalTo(Vector(
+          contains(
             s"""Running with
-              |      URL : https://storage.googleapis.com/mygration
-              | indexURL : https://storage.googleapis.com/mygration/index.txt
-              |   dryRun : false
-              |directory : ${testDir}
-              |  pattern : s///g""".stripMargin,
-            "curl -o ./target/downloads/test.txt someurl && checkStatusIn []"
-          )))
-
+               |               URL : https://storage.googleapis.com/mygration
+               |          indexURL : https://storage.googleapis.com/mygration/index.txt
+               |            dryRun : false
+               |         directory : ${testDir}
+               |  filename pattern : s///g""".stripMargin
+          )) &&
+          assert(output)(contains(s"mkdir -p $testDir")) &&
+          assert(output)(contains(s"# downloading https://storage.googleapis.com/mygration/backup-2.zip to directory ${testDir}")) &&
+          assert(output)(contains(s"# downloading https://storage.googleapis.com/mygration/backup-0.zip to directory ${testDir}")) &&
+          assert(output)(contains(s"# downloading https://storage.googleapis.com/mygration/backup-3.zip to directory ${testDir}")) &&
+          assert(output)(contains(s"# downloading https://storage.googleapis.com/mygration/backup-1.zip to directory ${testDir}")) &&
+          assert(exitCode)(equalTo(ExitCode.failure))
       }
     })
   }
